a1610a85c352650f8e882994c1842fa9a2680621
AbdulloRaximjonov <raximjonovabdullo8@gmail.com>
2025-06-19 15:52:24 +0500

    Initial project version

--- js/script.js
+++ js/script.js
@@ -51,4 +51,38 @@ openBtn.addEventListener('click', () => {
     })
 }
 
+const slides = document.querySelectorAll(".offer__slide")
+const prev = document.querySelector(".offer__slider-prev")
+const next = document.querySelector(".offer__slider-next")
+const current = document.querySelector("#current")
+const total = document.querySelector("#total")
+
+let index = 0
+
+total.textContent = slides.length
+current.textContent = index + 1
+
+function showSlide(i) {
+slides.forEach(slide => {
+    slide.classList.remove("show")
+})
+
+    slides[i].classList.add("show")
+
+    current.textContent = i + 1
+}
+
+showSlide(index)
+
+next.onclick = () => {
+    index = (index + 1) % slides.length
+    showSlide(index)
+}
+prev.onclick = () => {
+    index = (index - 1 + slides.length) % slides.length
+    showSlide(index)
+}
+
+
+
 
