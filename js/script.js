5b21ad607992aba618ae8be9e11dfabe39161428
AbdulloRaximjonov <raximjonovabdullo8@gmail.com>
2025-06-26 15:01:15 +0500

    w

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ const total = document.querySelector("#total")
 
 let index = 0
 
-total.textContent = slides.length
+
 current.textContent = index + 1
 
 function showSlide(i) {
@@ -67,9 +67,9 @@ slides.forEach(slide => {
     slide.classList.remove("show")
 })
 
-    slides[i].classList.add("show")
-
-    current.textContent = i + 1
+    slides[i].classList.add("show", "fade")
+total.textContent = String(slides.length).padStart(2, "0")
+    current.textContent = String(i + 1).padStart(2, "0")
 }
 
 showSlide(index)
@@ -81,6 +81,83 @@ next.onclick = () => {
 prev.onclick = () => {
     index = (index - 1 + slides.length) % slides.length
     showSlide(index)
+}
+
+
+const genderBtns = document.querySelectorAll("#gender .calculating__choose-item")
+
+const activityBtns = document.querySelectorAll("#activity.calculating__choose-item")
+
+const inputs = document.querySelectorAll("input.calculating__choose-item")
+
+const calculating__result = document.querySelector(".calculating__result span")
+
+let userData = {
+    gender: "woman",
+    height: 0,
+    weight: 0,
+    age: 0,
+    activity: 0
+
+
+}
+
+
+
+
+
+genderBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        genderBtns.forEach(el => el.classList.remove("calculating__choose-item_active"))
+        btn.classList.add("calculating__choose-item_active")
+        userData.gender = btn.getAttribute("data-gender")
+
+    })
+
+})
+activityBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        activityBtns.forEach(el => el.classList.remove("calculating__choose-item_active"))
+        
+        btn.classList.add("calculating__choose-item_active")
+        
+        userData.activity = btn.getAttribute("data-activity")
+        calcKkal(userData)
+
+    })
+
+})
+
+
+inputs.forEach(inp => {
+    inp.oninput = () => {
+        userData[inp.id] = inp.value
+
+
+
+
+
+    }
+})
+
+
+
+function calcKkal(data) {
+    let result = 0
+
+    if (userData.gender === "woman") {
+        result = 655.1 + (9.563 * data.weight) + (1.85 * data.height) - (4.676 * data.age)
+    } else {
+        result = 66.5 + (13.75 * data.weight) + (5.003 * data.height) - (6.775 * data.age)
+    }
+
+    result = Math.round(result * data.activity)
+
+    calculating__result.textContent = result
+
+    return result
+
+
 }
 
 
